22a499390970e9789eb01b80825c09e0b7a3ba5c
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-11-04 16:34:04 +1100

    New specs added to user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,16 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to advertisements_path
   end
 
   private
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -108,5 +108,58 @@ describe UsersController do
         end
       end
     end
+    context "is the owner of the account user" do
+      describe "GET edit" do
+        it "renders :edit template" do
+          get :edit, params: { id: user }
+          expect(response).to render_template(:edit)
+        end
+
+        it "assigns the requested user to template" do
+          get :edit, params: { id: user }
+          expect(assigns(:user)).to eq(user)
+        end
+      end
+      describe "PUT update" do
+        context "valid data" do
+          let(:valid_data) { FactoryGirl.attributes_for(:user, full_name: "Jon Snow") }
+
+          it "redirects to user#show" do
+            put :update, params: { id: user, user: valid_data }
+            expect(response).to redirect_to(user)
+          end
+          it "updates iuser in the database" do
+            put :update, params: { id: user, user: valid_data }
+            user.reload
+            expect(user.full_name).to eq("Jon Snow")
+          end
+        end
+        context "invalid data" do
+          let(:invalid_data) { FactoryGirl.attributes_for(:user, full_name: "", username: "jonsnow") }
+
+          it "renders :edit template" do
+            put :update, params: { id: user, user: invalid_data }
+            expect(response).to render_template(:edit)
+          end
+
+          it "doesn't update user in the database" do
+            put :update, params: { id: user, user: invalid_data }
+            user.reload
+            expect(user.username).not_to eq('jonsnow')
+          end
+        end
+      end
+      describe "DELETE destroy" do
+        it "redirects to user#index" do
+          delete :destroy, params: { id: user }
+          expect(response).to redirect_to(advertisements_path)
+        end
+
+        it "deletes user from database" do
+          delete :destroy, params: { id: user }
+          expect(User.exists?(user.id)).to be_falsy
+        end
+      end
+    end
   end
 end
