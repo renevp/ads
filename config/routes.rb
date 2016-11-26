9b303b79d7b1d483599682b9646ab6d6b125feb0
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-11-26 14:26:54 +1100

    Devise controllers added and other minor adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   resources :advertisements
   resources :users
+
 
   root to: 'welcome#index'
 end
