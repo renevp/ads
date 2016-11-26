a4cf5faab53b6827949980d8928207c688705d4b
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-11-26 23:29:45 +1100

    Adbvertiments are now filtered when the user is not active anymore.

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -17,15 +17,15 @@ class Advertisement < ApplicationRecord
   scope :published_and_sell, -> { where(status: 'published', ad_type: 'sell')  }
   scope :published_and_buy, -> { where(status: 'published', ad_type: 'buy')  }
   scope :ordered_by_price, -> { order(price_cents: :desc) }
-  # scope :active_user, -> { joins(:user).where(user: { status: :active}) }
+  scope :active_user, -> { joins(:user).where("users.status = 0") }
 
   class << self
-    def published_sell_ordered_by_price
-      published_and_sell.ordered_by_price
+    def published_sell
+      published_and_sell.active_user.ordered_by_price
     end
 
-    def published_and_buy_ordered_by_price
-      published_and_buy.ordered_by_price
+    def published_buy
+      published_and_buy.active_user.ordered_by_price
     end
   end
 
